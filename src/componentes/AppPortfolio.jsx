fd94cbb9e7089e17d7becdea41e64392b56b197d
Joseph <elsjoseph@hotmail.com>
2024-09-23 10:58:58 -0500

    tst

--- src/componentes/AppPortfolio.jsx
+++ src/componentes/AppPortfolio.jsx
@@ -7,7 +7,7 @@ const AppPortfolio = () => {
       <h3 id="portfolio">Portafolio</h3>
       <div id="proyecto">
         <div id="imagenProyecto">
-          <img src="src/assets/App.jpeg" />
+          <img src="src/assets/app.jpeg" />
         </div>
         <div id="informacion">
           <h4 id="titulo">Delivery App | Android & IOS</h4>
